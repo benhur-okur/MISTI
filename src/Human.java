e04dcba14f7839895eb7129728822929ccc9621c
benhur-okur <117393881+benhur-okur@users.noreply.github.com>
2023-05-14 19:28:50 +0300

    PROJECT HAS FINISHED
    LAST CHANGES FOR ERRORS

--- src/Human.java
+++ src/Human.java
@@ -7,13 +7,32 @@ public class Human extends Player{
 
     @Override
     protected int decidePlayCardIndex(Board board){
+        int selectCard = -1;
+        boolean trueIndex = false;
         // ToDo: Please select a card using index between 0 - ???
         // for( Card card: hand ){ print'te hand.getFace, hand.getSuit vs } )
         // seçtikten sonra da  index kullanarak Card return et
         Scanner sc = new Scanner(System.in);
 
-        System.out.printf("Please Select a Card in your hand use index between 0 - %d [both include] !%n", getHand().size()-1);
-        int selectCard = sc.nextInt();
+        do {
+            System.out.printf("Please Select a Card in your hand use index between 0 - %d [both include] !%n", getHand().size() - 1);
+            try {
+                selectCard = sc.nextInt();
+                if (selectCard >= 0 && selectCard <= (getHand().size()-1)) {
+                    trueIndex = true;
+                } else {
+                    System.out.printf("Please Select a Card in your hand use index between 0 - %d [both include] !%n", getHand().size() - 1);                }
+            } catch (Exception e) {
+                System.out.printf("Please Select a Card in your hand use index between 0 - %d [both include] !%n", getHand().size() - 1);
+                sc.next(); // clear the input buffer
+            }
+        } while (!trueIndex);
+
+        /*while (selectCard < 0 || selectCard > getHand().size()-1) {
+
+            System.out.printf("Please Select a Card in your hand use index between 0 - %d [both include] !%n", getHand().size() - 1);
+            selectCard = Integer.parseInt(sc.nextLine());
+        }*/
 
         return selectCard;
     }
